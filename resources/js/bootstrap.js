b12b0c9bd8dc536829ad6ade1b3cb2a4f5de3efb
Marcon Sibal <marcon.sibal@gmail.com>
2024-04-01 21:18:11 +0100

    Applies changes and installs laravel breeze vue

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,7 @@
-window._ = require('lodash');
+import has from 'lodash/has';
+import axios from 'axios';
+
+window._ = has;
 
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
@@ -6,7 +9,7 @@ window._ = require('lodash');
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 
-window.axios = require('axios');
+window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
